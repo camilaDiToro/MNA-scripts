105d1ecd3af188dd5d3398dae351d0c585d4f8af
lvittor <lvittor@itba.edu.ar>
2022-09-14 20:18:59 -0300

    chore: add scripts/

--- scripts/estimar_autovalor_dominante_y_su_autovector.m
+++ scripts/estimar_autovalor_dominante_y_su_autovector.m
@@ -1,5 +1,3 @@
-% Usado para hacer scripting
-
 format long
 
 
@@ -7,4 +5,4 @@ A = [1 -1 0; 2 4 -2; 0 -1 1]
 x0 = [1; -0.5; 1]
 [v, lambda, niter] = apower(A, v0=x0, tol=1e-10, max_iter=1000)
 
-% [P D] = eig(A)
+[P D] = eig(A)
