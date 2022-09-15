46de3fb62ae0b12ed5df7df18906848da32adb08
lvittor <lvittor@itba.edu.ar>
2022-09-15 00:28:07 -0300

    chore: add findwraval

--- FINDQRAVAL.m
+++ FINDQRAVAL.m
@@ -1,3 +1,7 @@
+% Calcula por el metodo QR todos los autovalores de una matriz dada
+%
+% Es la implementacion de eig() de octave. SIEMPRE comparar contra eig() los resultados obtenidos!
+
 function a=FINDQRAVAL(A, tol, max_iter)
   if (nargin==1)
      tol=1e-6; max_iter =10000;
@@ -20,3 +24,8 @@ function a=FINDQRAVAL(A, tol, max_iter)
     fprintf('Número de iteraciones=%d \n' ,k-1)
   endif
 endfunction
+
+% A = [1 -1 0; 2 4 -2; 0 -1 1]
+% a = FINDQRAVAL(A, tol=1e-10, max_iter=1000)
+% printf("\n\nComparo los autovalores obtenidos con eig()\n\n")
+% eig(A)
