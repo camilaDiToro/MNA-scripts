5f75d866ac466baf4e06bc8061a9f797f7ed5cf3
lvittor <lvittor@itba.edu.ar>
2022-09-14 23:00:14 -0300

    chore: add comments

--- script.m
+++ script.m
@@ -2,9 +2,37 @@
 
 format long
 
+% cc @mbaiges
+function [V, lambda, it] = pow_met(A, v0, n=1000, tol=1e-10, tol_typ="lambda")
 
+    V0 = v0;
+    V0 = V0/norm(V0);
+
+    V = [V0'];
+    lambda = [];
+
+    for it=1:n
+        new_V = A*V(it,:)';
+        new_lambda = dot(new_V, V(it,:));
+        lambda = [lambda; new_lambda];
+        new_V = new_V/norm(new_V);
+        V = [V; new_V'];
+
+        if it > 1
+            if strcmp(tol_typ, "lambda") == 1
+                dif = abs(new_lambda - lambda(it-1));
+            elseif strcmp(tol_typ, "full") == 1
+                dif = norm(A*new_V - lambda(it-1)*new_V);
+            endif
+            if dif < tol
+                break
+            endif
+        endif
+    endfor
+
+endfunction
 A = [1 -1 0; 2 4 -2; 0 -1 1]
 x0 = [1; -0.5; 1]
-[v, lambda, niter] = APOWER(A, v0=x0, tol=1e-10, max_iter=1000)
+[v, lambda, niter] = pow_met(A, v0=x0, n=1000, tol=1e-10, tol_typ="full")
 
 % [P D] = eig(A)
--- scripts/estimar_autovalor_dominante_y_menor.m
+++ scripts/estimar_autovalor_dominante_y_menor.m
@@ -16,3 +16,11 @@ printf("Calculo el menor (en modulo) autovalor")
 printf("\n\n\n\n")
 [v, lambda, niter] = ISPOWER(A, v0, tol=1e-5, max_iter=1000)
 
+% printf("\n\n\n\n")
+% printf("Calculo el autovalor dominante con diferentes condiciones iniciales")
+% printf("\n\n\n\n")
+
+% for i=1:50
+%     x0 = 2^(i)*[1; -3; 1] + [1; 0; 1];
+%     [v, lambda, niter] = APOWER(A, x0, tol=1e-10, max_iter=1000)
+% endfor
